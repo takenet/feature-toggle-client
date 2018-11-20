c2868a12b48595480a836613c540ca6fac8b15ca
samwx <samuelmartins.sw@gmail.com>
2018-11-20 10:53:33 -0200

    fix: Set UserAccount properties as optional

--- src/types/UserAccount.ts
+++ src/types/UserAccount.ts
@@ -1,14 +1,14 @@
 export class UserAccount {
-  public id: number;
-  public address: string;
-  public email: string;
-  public fullName: string;
-  public password: string;
-  public phoneNumber: string;
+  public id?: number;
+  public address?: string;
+  public email?: string;
+  public fullName?: string;
+  public password?: string;
+  public phoneNumber?: string;
   public whatsAppPhoneNumber?: string;
-  public culture: string;
-  public isPending: boolean;
-  public company: string;
+  public culture?: string;
+  public isPending?: boolean;
+  public company?: string;
   public position?: string;
   public expiration?: string;
   public city?: string;
